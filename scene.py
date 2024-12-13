1efcc4c580160223d35efeab2adec92470dce609
Artem Savranenko <savranenko.as@edu.spbstu.ru>
2024-12-13 06:26:19 +0300

    Delete unwanted class for directed emitter

--- scene.py
+++ scene.py
@@ -52,45 +52,6 @@ class Scene:
         """Добавляем объекты (кубы, конусы и т.д.) в сцену."""
         self.objects.append(obj)
 
-    def add_animation(self, animation):
-        """Добавляем анимацию в сцену."""
-        self.animations.append(animation)
-
-    @staticmethod
-    def draw_axes():
-        """Отрисовка осей координат."""
-        glBegin(GL_LINES)
-
-        # Оси
-        glColor3f(1, 0, 0)  # Ось X
-        glVertex3f(-10.0, 0.0, 0.0)
-        glVertex3f(10.0, 0.0, 0.0)
-
-        glColor3f(0, 1, 0)  # Ось Y
-        glVertex3f(0.0, -10.0, 0.0)
-        glVertex3f(0.0, 10.0, 0.0)
-
-        glColor3f(0, 0, 1)  # Ось Z
-        glVertex3f(0.0, 0.0, -10.0)
-        glVertex3f(0.0, 0.0, 10.0)
-
-        glEnd()
-
-    @staticmethod
-    def draw_grid():
-        """Отрисовка сетки."""
-        glColor3f(0.5, 0.5, 0.5)
-        glBegin(GL_LINES)
-
-        for i in range(-10, 11):
-            glVertex3f(i, 0, -10)
-            glVertex3f(i, 0, 10)
-            glVertex3f(-10, 0, i)
-            glVertex3f(10, 0, i)
-
-        glEnd()
-
-
     def distance_to_camera(self, position):
         """Вычисляет расстояние от позиции до камеры."""
         cam_pos = self.camera.position
